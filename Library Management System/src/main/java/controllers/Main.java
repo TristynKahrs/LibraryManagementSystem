ac6bc53f66520ccf419f3e634bdae88ea8f173e0
TristynKahrs <79658190+TristynKahrs@users.noreply.github.com>
2021-08-16 16:46:41 -0600

    GUI controllers created

--- Library Management System/src/main/java/controllers/Main.java
+++ Library Management System/src/main/java/controllers/Main.java
@@ -3,20 +3,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import views.CLI;
 import java.io.IOException;
 
-public class Main  extends Application {
+public class Main extends Application {
 
     public static void main(String[] args){
-        System.out.println("Hello Chris_1");
         try {
-//            CLI.startupMenu();
             launch();
         }catch(Exception e){
             e.printStackTrace();
         }
-        System.out.println("Hello Chris_2");
     }
 
     @Override
