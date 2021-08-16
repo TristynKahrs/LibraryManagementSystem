c861674e868c179d4ede310341321f54de3770fb
TristynKahrs <79658190+TristynKahrs@users.noreply.github.com>
2021-08-16 16:54:31 -0600

    CreateAccount basic implementation

--- Library Management System/src/main/java/views/UserGUIController.java
+++ Library Management System/src/main/java/views/UserGUIController.java
@@ -1,4 +1,13 @@
 package views;
 
-public class UserGUIController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserGUIController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // TODO Auto-generated method stub
+    }
 }
