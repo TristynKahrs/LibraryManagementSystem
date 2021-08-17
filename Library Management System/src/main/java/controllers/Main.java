cf5d795c10482e33f3ce4db1808e9f0854fc45a6
TristynKahrs <79658190+TristynKahrs@users.noreply.github.com>
2021-08-17 15:58:42 -0600

    LoginGUI basic functionality

--- Library Management System/src/main/java/controllers/Main.java
+++ Library Management System/src/main/java/controllers/Main.java
@@ -1,12 +1,5 @@
 package controllers;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import views.LoginGUIController;
-
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args){
--- Library Management System/src/main/java/views/LoginGUIController.java
+++ Library Management System/src/main/java/views/LoginGUIController.java
@@ -1,29 +1,31 @@
 package views;
 
-import controllers.Main;
+import controllers.AccountManagement;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import java.io.IOException;
+import models.User;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginGUIController extends Application implements Initializable  {
+public class LoginGUIController extends Application implements Initializable {
     Stage window;
 
     @Override
     public void start(Stage stage) throws IOException {
         window = stage;
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main-Menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginGUIController.class.getResource("Main-Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         window.setTitle("Library Management System!");
         window.setScene(scene);
@@ -32,27 +34,38 @@ public class LoginGUIController extends Application implements Initializable  {
 
     //Login
     @FXML
-    public AnchorPane MainPane;
+    public Pane MainPane;
     public TextField LoginUsername;
-    public TextField LoginPassword;
+    public PasswordField LoginPassword;
 
     @FXML
     public Button LogInBtn;
 
     public void onLoginButtonClick(ActionEvent event) {
-        //check that both textfields are not empty, then try to login
+        String username = LoginUsername.getText();
+        String password = LoginPassword.getText();
+        if (!username.equals("") && !password.equals("")) {
+            try {
+                User activeUser = AccountManagement.login(username, password);
+//                TODO go to main menu controller/browse-pane
+            } catch (SecurityException se) {
+                // TODO make a label that says try again when failing login
+            }
+        }
     }
 
     @FXML
     public Label CreateAccountLbl;
+
     public void onCreateAccountLink(MouseEvent event) {
-        //go to create account pane
+        // TODO go to create account pane
     }
 
     @FXML
     public Label ForgotPasswordLbl;
+
     public void onForgotPasswordClick(MouseEvent event) {
-        // go to forgot password
+        // TODO go to forgot password
     }
     //Login close
 
@@ -67,8 +80,25 @@ public class LoginGUIController extends Application implements Initializable  {
 
     @FXML
     public Button CreateBtn;
-    public void onCreateAccountBtn(ActionEvent event) {
 
+    public void onCreateAccountBtn(ActionEvent event) {
+        String fullName = FullName.getText();
+        String userName = Username.getText();
+        String password = Password.getText();
+        String passwordConfirmation = PasswordConfirmation.getText();
+        if (!fullName.equals("") && !userName.equals("") && !password.equals("") && !passwordConfirmation.equals("")) {
+            if (password.equals(passwordConfirmation)) {
+                try {
+                    AccountManagement.createAccount(fullName, userName, password);
+                    User activeUser = AccountManagement.login(userName, password);
+                    //TODO go to main menu controller/browse-pane (login for them)
+                } catch (SecurityException se) {
+                    //TODO label that says that username is taken
+                }
+            } else {
+                //TODO make a label that says passwords did not match
+            }
+        }
     }
     //CreateAccount close
 
@@ -80,15 +110,24 @@ public class LoginGUIController extends Application implements Initializable  {
     public Label PasswordRetrieval;
 
     @FXML
-    public void btnSubmit(ActionEvent event) {
+    public Button RecoverSubmitBtn;
 
+    public void btnSubmit(ActionEvent event) {
+        String fullName = fullNameSearch.getText();
+        String userName = usernameSearch.getText();
+        if(!fullName.equals("") && !userName.equals("")) {
+            try{
+                String password = AccountManagement.recoverPassword(fullName, userName);
+                PasswordRetrieval.setText(password);
+            }catch (SecurityException se) {
+                PasswordRetrieval.setText("That name and username don't go together, try again.");
+            }
+        }
     }
     //Recover password close
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // TODO Auto-generated method stub
     }
-
 }
