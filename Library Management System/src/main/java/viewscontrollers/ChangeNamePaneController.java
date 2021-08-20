a95484e0b8fa89c997f2049e42f707e805c037fc
TristynKahrs <79658190+TristynKahrs@users.noreply.github.com>
2021-08-20 12:15:55 -0600

    changePassword

--- Library Management System/src/main/java/viewscontrollers/ChangeNamePaneController.java
+++ Library Management System/src/main/java/viewscontrollers/ChangeNamePaneController.java
@@ -1,14 +1,41 @@
 package viewscontrollers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ChangeNamePaneController implements Initializable {
-    //TODO implement fxml fields
+
+    public TextField txtUsername;
+
+    public TextField txtPassword;
+
+    public TextField txtNewFullName;
+
+    public Button btnSubmit;
+    public void onClickSubmit(ActionEvent event) {
+        String strUsername = txtUsername.getText();
+        String strPassword = txtPassword.getText();
+        String strNewFullName = txtNewFullName.getText();
+        if(!strUsername.equals("") && !strPassword.equals("") && !strNewFullName.equals("")) {
+
+        }else{
+            lblConfirmation.setText("Must enter all fields");
+        }
+    }
+
+    public Label lblConfirmation;
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
 }
