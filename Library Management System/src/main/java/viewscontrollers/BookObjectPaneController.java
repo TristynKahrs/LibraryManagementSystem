024208474c7b7b2b4990ef07f21b142f1403ae0d
rbrunney <rbrunney@student.neumont.edu>
2021-08-23 14:48:11 -0600

    Images for book controllers completed

--- Library Management System/src/main/java/viewscontrollers/BookObjectPaneController.java
+++ Library Management System/src/main/java/viewscontrollers/BookObjectPaneController.java
@@ -7,12 +7,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Window;
 import models.Book;
 import models.DisplayBooks;
 import models.User;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -42,6 +45,9 @@ public class BookObjectPaneController implements Initializable {
     public Label lblAuthor;
 
     @FXML
+    public ImageView imgBook;
+
+    @FXML
     public Button btnCheckOut;
 
     @FXML
@@ -59,7 +65,7 @@ public class BookObjectPaneController implements Initializable {
         User user = ChangeScene.receiveData(event);
         Window owner= btnCheckOut.getScene().getWindow();
         book = new Book(DatabaseOperations.getBook(lblTitle.getText().substring(7), lblAuthor.getText().substring(8)));
-        
+
         try {
             LibraryManagement.checkOut(user, book);
             DisplayBooks.setAllBooks();
@@ -102,6 +108,9 @@ public class BookObjectPaneController implements Initializable {
         if (book != null) {
             lblTitle.setText("Title: " + book.getTitle());
             lblAuthor.setText("Author: " + book.getAuthor());
+            File bookFile = new File("src//main//resources//images//" + book.getPrimaryKey() + ".jpg");
+            Image bookIMG = new Image(bookFile.toURI().toString());
+            imgBook.setImage(bookIMG);
         }
     }
 }
